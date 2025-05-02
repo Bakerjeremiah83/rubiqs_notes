337dd7236d0743d443aedaf97521a3798aa57eee
Your Name <you@example.com>
2025-05-01 20:43:34 -0600

    Update Client ID for Rubiqs Notes LTI tool in Moodle

--- utils/lti_platforms.py
+++ utils/lti_platforms.py
@@ -1,6 +1,6 @@
 PLATFORMS = {
     "moodle": {
-        "client_id": "BAalzjoELaJJiQF",
+        "client_id": "q4ErBvQ5NU9mle6",
         "auth_login_url": "https://rubiqs-suite.moodlecloud.com/mod/lti/auth.php",
         "auth_token_url": "https://rubiqs-suite.moodlecloud.com/mod/lti/token.php",
         "key_set_url": "https://rubiqs-suite.moodlecloud.com/mod/lti/certs.php",
